f67d0c6f3fa0549fd64d6edb1c6fbbb93a7b9847
Gabriel Paiva <gabriel.paiva0@icloud.com>
2025-05-30 10:42:11 -0300

    Remove OptimizedGrainOverlay.swift; functionalities consolidated into GrainEffect.swift to eliminate duplication.

--- Granulay/OptimizedGrainOverlay.swift
+++
@@ -1,6 +0,0 @@
-import SwiftUI
-import CoreImage
-import CoreImage.CIFilterBuiltins
-
-// Este arquivo foi consolidado com GrainEffect.swift para evitar duplicação
-// Todas as funcionalidades foram movidas para o arquivo principal GrainEffect.swift
