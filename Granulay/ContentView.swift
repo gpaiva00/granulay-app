d0659f5ba0b27097b1fa1227c285444e2795ea70
Gabriel Paiva <gabriel.paiva0@icloud.com>
2025-05-30 13:27:16 -0300

    Substitui ícone da câmera por ícone de configurações na ContentView e ajusta suas propriedades de exibição.

--- Granulay/ContentView.swift
+++ Granulay/ContentView.swift
@@ -3,9 +3,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "camera.filters")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Image("SettingsViewIcon")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+                .foregroundColor(.accentColor)
             Text("Granulay")
         }
         .padding()
